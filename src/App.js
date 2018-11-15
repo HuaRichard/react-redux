32eee0b7bc71c21885ebf1d0baeb8c20b7d4a492
Richard Hua <huachao0349@gmail.com>
2018-11-16 01:17:48 +1100

    add useReducer counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import { useState } from 'react';
-import React from 'react';
+import React, { useReducer } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import Counter from './components/Counter';
+import { reducer } from './reducer';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
+
   return (
-    <div>
+    <div className="App">
       <Counter
-        value={count}
-        onIncrement={() => setCount(count + 1)}
-        onDecrement={() => setCount(count - 1)} />
+        value={state}
+        onIncrement={() => dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => dispatch({ type: 'DECREMENT' })}
+      />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
